731390db2fdc3dc5b86211ded7f759ae585e5fc0
habibeorkshop <thehabibgamerz2007@gmail.com>
2026-05-08 14:56:54 +0530

    Update event.py

--- cogs/event.py
+++ cogs/event.py
@@ -8,7 +8,7 @@ import re
 
 # ================= CONFIG =================
 
-STAFF_ROLE_ID = 1389824693388837035  # Staff Role ID
+STAFF_ROLE_ID = 1493559145876557955  # Staff Role ID
 GUILD_ID = 1493552564799672320
 
 IST = timezone(timedelta(hours=5, minutes=30))
